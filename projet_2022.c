d09c6852b8ca0b52d09adaaebf9562c3ac361149
SenseiSamy <samy.naji2003@gmail.com>
2022-03-28 16:11:18 +0200

    Ecriture de la base du code

--- projet_2022.c
+++ projet_2022.c
@@ -1,3 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*-- Fonction de sauvegardes --*/
+int* lire_chessboard(FILE *nom_fichier, int chessboard[5][5]){
+    /*Lit un fichier stockant le chessboard et renvoie un Array2D décrivant les valeurs des cellules de la grille*/
+}
+
+void ecrire_chessboard(int chessboard[5][5], FILE *nom_fichier){
+    /*Sauvegarde un Array2D dans un fichier/*
+}
+
+/*-- Fonctions de jeu --*/
+int sans_conflit(Position orig, Position fin, int chessboard[5][5]){
+    /*Vérifie si un mouvement d'un reine est possible, renvoie 0 si le déplacement est possible, renvoie 1 sinon*/
+}
+
+int winning(int chessboard[5][5]){
+    /*Vérifie si un joueur a gagné en vérifiant le plateau*/
+}
+
 int main(void){
-    
+    return 0;
 }
